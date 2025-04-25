58d4ebf7c5d783d3a1197edac20f14a067286076
Megan Brown <bkmegan@bgsu.edu>
2025-04-25 01:31:36 -0400

    added equivalence partition tests for DateStringLookup

--- src/test/java/org/apache/commons/text/lookup/DateStringLookupPartitionTest.java
+++ src/test/java/org/apache/commons/text/lookup/DateStringLookupPartitionTest.java
@@ -17,7 +17,6 @@
 
 package org.apache.commons.text.lookup;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
